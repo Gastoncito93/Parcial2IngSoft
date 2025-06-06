de74a32e025236f3958f86229db6bc2474099e68
Gastoncito93 <gastonoscarsosa@gmail.com>
2025-06-06 17:10:02 -0300

    code mod

--- Parcial2IngSoft/src/parcial2ingsoft/Parcial2IngSoft.java
+++ Parcial2IngSoft/src/parcial2ingsoft/Parcial2IngSoft.java
@@ -10,11 +10,27 @@ package parcial2ingsoft;
  */
 public class Parcial2IngSoft {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Vehiculo vehiculo = new Vehiculo("ABC123", "Toyota", "Corolla", "Blanco");
+        Wallet wallet = new Wallet();
+        Conductor conductor = new Conductor(1, "Carlos", "carlos@mail.com", "123456789", "pass123",
+                                            "LIC123", vehiculo, wallet);
+
+        Pasajero pasajero = new Pasajero(2, "Ana", "ana@mail.com", "987654321", "clave123");
+        Viaje viaje = new Viaje("Av. Siempre Viva 123", "Calle Falsa 456", pasajero);
+
+        // Simular flujo de Uber Driver
+        conductor.recibirSolicitud(viaje);
+        conductor.verInfoViaje();
+        conductor.aceptarSolicitud();
+        conductor.llegarAlPasajero();
+        conductor.iniciarViaje();
+        conductor.realizarViaje();
+
+        Calificacion califPasajero = new Calificacion(5, "Muy buen viaje", "06/06/2025");
+        Calificacion califConductor = new Calificacion(5, "Excelente pasajero", "06/06/2025");
+
+        conductor.finalizarViaje(califPasajero, califConductor);
     }
-    
 }
+    
